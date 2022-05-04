a388d3a96cdb80e797f965ada7629affd33e9d94
JongKuk Seo <jongkuk.dev@gmail.com>
2022-05-04 23:57:49 +0900

    Refactor: 설정 클래스 중복제거 및 역할과 구현파악시 가독성 향상

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,5 +1,6 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
@@ -9,13 +10,22 @@ import hello.core.order.OrderServiceImpl;
 
 
 //역할과 책임 분리: 실제 동작에 필요한 객체를 생성하고 연결하는 역할. OOP SOLID 원칙 중  DIP 원칙 위반 해소
+//중복 해소 / 애플리케이션에서 역할에 따른 구현이 잘보이게 리팩터링(큰 그림이 한눈에 보이게) 
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
     public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new RateDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    private MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new RateDiscountPolicy();
     }
 }
