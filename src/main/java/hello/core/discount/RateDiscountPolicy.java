9c10b3f392df7cdba04942eabc64879de6a0196f
JongKuk Seo <jongkuk.dev@gmail.com>
2022-05-16 23:54:21 +0900

    Feat: 조회된 스프링빈 모두 관리

--- src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -1,5 +1,6 @@
 package hello.core.discount;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -7,8 +8,9 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-@Qualifier("mainDiscountPolicy")
+//@Qualifier("mainDiscountPolicy")   //문자는 컴파일시 타입체크 불가능
 //@Primary
+@MainDiscountPolicy //내가 직접 만든 어노테이션을 이용, 컴파일시 오류 체크 및 해당 소스 코드 위치 추적 가능
 public class RateDiscountPolicy implements DiscountPolicy{
 
     private int discountPercent = 10;
