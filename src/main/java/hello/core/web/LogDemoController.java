99e42ab7d6cdfbe242395fd1edf28f6bbb472aaa
JongKuk Seo <jongkuk.dev@gmail.com>
2022-06-01 16:16:58 +0900

    ObjectProvider 적용

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -2,6 +2,7 @@ package hello.core.web;
 
 import hello.core.common.MyLogger;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,15 +14,18 @@ import javax.servlet.http.HttpServletRequest;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final MyLogger myLogger;   //스프링 시작 단계에서는 request가 들어오지 않았기 때문에 빈 생성되지 않았음. 오류 발생. 프로바이더가 필요하다!
+    private final ObjectProvider<MyLogger> myLoggerProvider;   //스프링 시작 단계에서는 request가 들어오지 않았기 때문에 빈 생성되지 않았음. 오류 발생. 프로바이더가 필요하다!
 
     @RequestMapping("log-demo")
     @ResponseBody
-    public String logDemo(HttpServletRequest request){
-
+    public String logDemo(HttpServletRequest request) throws InterruptedException {
         String requestUrl = request.getRequestURL().toString();
+        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.setRequestUrl(requestUrl);
         myLogger.log("controller test");
+
+        Thread.sleep(1000);
+
         logDemoService.logic("testId");
 
         return "okay";
